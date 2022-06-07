9a069460938caa473516be8a105e0848fa803f77
Kamran Pulatov <84636918+WrldEngine@users.noreply.github.com>
2022-06-07 23:05:23 +0500

    Update scr.py

--- scr.py
+++ scr.py
@@ -1,2 +1,10 @@
 from browser import *
-lst = html.strong()
+import javascript
+
+def time(event):
+    date = javascript.Date.new()
+    document["outp"].text = "Сегодня {}-{:02}-{:02} \n И время: {:02}:{:02}:{:02}".format(
+        date.getFullYear(), date.getMonth()+1, date.getDate(),
+        date.getHours(), date.getMinutes(), date.getSeconds())
+
+document["btn"].bind("click", time)
