6aa15dcc12c6a4779443ede07632c6c35a9f85ea
Kamran Pulatov <84636918+WrldEngine@users.noreply.github.com>
2022-06-07 23:34:37 +0500

    Update scr.py

--- scr.py
+++ scr.py
@@ -1,6 +1,6 @@
 from browser import *
 import javascript
 date = javascript.Date.new()
-document["outp"].text = "Сегодня {}-{:02}-{:02} \n И время: {:02}:{:02}:{:02}".format(
+document["outp"].text = "Сегодня {}-{:02}-{:02} \n И время: {:02}:{:02}".format(
     date.getFullYear(), date.getMonth()+1, date.getDate(),
-    date.getHours(), date.getMinutes(), date.getSeconds())
+    date.getHours(), date.getMinutes())
